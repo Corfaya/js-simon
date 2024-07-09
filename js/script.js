cddad9a3625749621dfef9578d3035ea1b696bf7
Corfaya <lauracorrado0696@gmail.com>
2024-07-09 19:28:05 +0200

    [UPD] readme update. Parameter added and while loop started with random number generation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // FUNCTIONS
-function numbersGenerator() {
+function numbersGenerator(array) {
     let ul = document.querySelector(".random");
+    while (array.length < 5) {
+        let random = Math.floor(Math.random() * 100 + 1);
+      }
   }
