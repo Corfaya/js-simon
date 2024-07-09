51c04c46cb71f72ee41f82c3980ba1089eb54f61
Corfaya <lauracorrado0696@gmail.com>
2024-07-09 19:30:36 +0200

    [UPD] li with random numbers created and added on html

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ function numbersGenerator(array) {
         let random = Math.floor(Math.random() * 100 + 1);
         if (!array.includes(random)) {
             array.push(random);
+            let li = document.createElement("li");
+            li.innerText = random;
+            ul.append(li);
           }
       }
       console.log(array);
