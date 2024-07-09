faa7121fb39661360b54ace6a5b76b6952f77edc
Corfaya <lauracorrado0696@gmail.com>
2024-07-09 19:34:56 +0200

    [UPD] Second function for user array recovery = empty array + for loop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 // FUNCTIONS
 function numbersGenerator(array) {
-    let ul = document.querySelector(".random");
-    while (array.length < 5) {
-        let random = Math.floor(Math.random() * 100 + 1);
-        if (!array.includes(random)) {
-            array.push(random);
-            let li = document.createElement("li");
-            li.innerText = random;
-            ul.append(li);
-          }
-      }
-      console.log(array);
+  let ul = document.querySelector(".random");
+  while (array.length < 5) {
+    let random = Math.floor(Math.random() * 100 + 1);
+    if (!array.includes(random)) {
+      array.push(random);
+      let li = document.createElement("li");
+      li.innerText = random;
+      ul.append(li);
+    }
   }
+  console.log(array);
+}
+
+function userNumbersRecovery() {
+    let userArray = [];
+    for (let k = 0; k < 5; k++) {
+        
+    }
+}
