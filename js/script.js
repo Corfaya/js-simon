c8c2ae98dd5af492953a5148f107d2df15089602
Corfaya <lauracorrado0696@gmail.com>
2024-07-09 19:29:30 +0200

    [UPD] if random number isn't in the array, push it in

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ function numbersGenerator(array) {
     let ul = document.querySelector(".random");
     while (array.length < 5) {
         let random = Math.floor(Math.random() * 100 + 1);
+        if (!array.includes(random)) {
+            array.push(random);
+          }
       }
+      console.log(array);
   }
